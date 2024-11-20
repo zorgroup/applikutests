4f90a0ad24826c1a28ca29b4ea77a2b7a1277933
Muhammad Mudassir <m.mudassir2015@gmail.com>
2024-11-20 16:17:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import stressinjector as injector
 
 # Stress CPU for a specified duration
-injector.CPUStress(seconds=960)  # Stresses CPU for 60 seconds
+injector.CPUStress(seconds=1960)  # Stresses CPU for 60 seconds
 
 # Stress Memory by allocating a specified amount
-injector.MemoryStress(gigabytes=5)  # Allocates 2 GB of memory
+injector.MemoryStress(gigabytes=15)  # Allocates 2 GB of memory

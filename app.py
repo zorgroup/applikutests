9005ffc8442930687b63afc88ecfc7492a9566d2
Muhammad Mudassir <m.mudassir2015@gmail.com>
2024-11-20 13:22:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import time
+import os
+DATABASE_URL = os.environ['hello']
+
 count=1
 while True:
     count=count+1
-    print("Hello ",count)
+    print("Hello ",count," ", DATABASE_URL)
     time.sleep(2)
